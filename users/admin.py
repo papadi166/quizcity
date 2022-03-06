467640140a6a91128b00636651e380dbb829e028
papadi166 <papadi166@gmail.com>
2022-03-06 22:52:14 +0100

    repair

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from .models import UserModel, FavouriteQuiz
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
 
-admin.site.register(UserModel)
+# Register your models here.
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        model = UserModel
+        fields = ('username', 'first_name' , 'last_name', )
+
+
+class UserAdmin(UserAdmin):
+    add_form = UserCreateForm
+    prepopulated_fields = {'username': ('first_name' , 'last_name', )}
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'username', 'password1', 'password2', ),
+        }),
+    )
+
+
+# Re-register UserAdmin
+
+
+admin.site.register(UserModel, UserAdmin)
 admin.site.register(FavouriteQuiz)
