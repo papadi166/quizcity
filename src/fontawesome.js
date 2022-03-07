b45243d12d61a4ee4be880d1d95282b37326d63b
papadi166 <41450410+papadi166@users.noreply.github.com>
2022-03-07 12:56:12 +0100

    Delete src directory

--- src/fontawesome.js
+++
@@ -1,9 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-import { faChess, faClock, faQuestion, faPercent, faBook, faMagnifyingGlass, faHeart, faUserGroup, faAnglesRight, faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons'
-
-
-library.add(faUserSecret)
-library.add(faSpinner)
-library.add(faChess, faClock, faQuestion, faPercent, faBook, faMagnifyingGlass, faHeart, faUserGroup,  faAnglesRight, faAngleDoubleLeft)
