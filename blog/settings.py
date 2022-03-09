bd0e966b2864fe7a90676a762920d99e509f2e9c
papadi166 <papadi166@gmail.com>
2022-03-09 01:56:12 +0100

    settings app change

--- blog/settings.py
+++ blog/settings.py
@@ -229,7 +229,7 @@ DATABASES['default'].update(db_from_env)
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ['https://quizcity.net', 'http://10.10.10.23']
+CSRF_TRUSTED_ORIGINS = ['http://quizcity.net', 'http://10.10.10.23']
 CORS_ORIGIN_WHITELIST = (
     'https://quizcity.net',
     'http://10.10.10.23',
