e0a12f23903dcbfff24ddfc5b8795a62dc59e1b8
papadi166 <papadi166@gmail.com>
2022-03-12 18:58:21 +0100

    optimization2

--- blog/settings.py
+++ blog/settings.py
@@ -294,11 +294,11 @@ os.environ['HTTPS'] = "on"
 DEBUG = True
 ALLOWED_HOSTS = ['quizcity2.herokuapp.com', 'www.quizcity.net']
 
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ALLOW_CREDENTIALS = True
+#CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = ['https://www.quizcity.net', 'http://10.10.10.23:8001']
 CORS_ORIGIN_WHITELIST = (
     'https://www.quizcity.net',
     'http://10.10.10.23:8001',
 )
-CORS_REPLACE_HTTPS_REFERER = False
+#CORS_REPLACE_HTTPS_REFERER = False
