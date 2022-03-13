41c6e4b66202fea5c70668ac671cb4de3ea8840a
papadi166 <papadi166@gmail.com>
2022-03-13 21:08:35 +0100

    x

---
+++ staticfiles/src/vue/dist/js/chunk-2d221475.c162850c99be.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d221475"],{ca56:function(n,t,c){"use strict";c.r(t);var e=c("f2bf");function i(n,t,c,i,s,o){return Object(e["s"])(),Object(e["f"])("h1",null,"this is notifications page")}var s={},o=c("6b0d"),a=c.n(o);const r=a()(s,[["render",i]]);t["default"]=r}}]);
+//# sourceMappingURL=chunk-2d221475.js.4db956e39fe8.map
