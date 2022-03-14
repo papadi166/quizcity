c63aaa800e5cfeabb00afc81333f5e7f0eab03ad
papadi166 <papadi166@gmail.com>
2022-03-14 23:52:26 +0100

    l

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -8,12 +8,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/staticfiles")
-application.add_files("/staticfiles", prefix="more-files/")
+
