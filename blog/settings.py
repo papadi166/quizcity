00db7b4f710c27d08c0cdeafbda422cf32c31d15
papadi166 <papadi166@gmail.com>
2022-03-14 02:09:47 +0100

    x

--- blog/settings.py
+++ blog/settings.py
@@ -208,8 +208,7 @@ ACCOUNT_EMAIL_REQUIRED = (True)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.IsAuthenticated',
--- quizes/views.py
+++ quizes/views.py
@@ -45,7 +45,7 @@ def quiz_data_view(request, pk):
 class QuizDetailViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all().order_by('-created_at')
     lookup_field = "slug"
-    serializer_class = QuizDetailSerializer(queryset, many=True)
+    serializer_class = QuizDetailSerializer
     #permission_classes = [IsAuthenticated]
 
 
