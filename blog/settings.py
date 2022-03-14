eb10f017d95b62191b9f2421a2e7bc58f596a166
papadi166 <papadi166@gmail.com>
2022-03-14 02:18:35 +0100

    x

--- blog/settings.py
+++ blog/settings.py
@@ -208,7 +208,8 @@ ACCOUNT_EMAIL_REQUIRED = (True)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.IsAuthenticated',
