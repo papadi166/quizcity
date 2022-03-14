23894002b94602c251ff97639088c225ef08ebce
papadi166 <papadi166@gmail.com>
2022-03-14 02:29:16 +0100

    user reciever create auth token

--- users/models.py
+++ users/models.py
@@ -4,6 +4,11 @@ from quizes.models import Quiz
 from django.conf import settings
 import uuid
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
 
@@ -20,3 +25,10 @@ class FavouriteQuiz(models.Model):
 
         class Meta:
                 verbose_name = "Favourite quizes"
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+                Token.objects.create(user=instance)
+    
