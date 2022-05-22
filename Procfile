2fcfd0ece00f91b6c4de431f52ae195f4586b0bd
papadi166 <papadi166@gmail.com>
2022-05-22 20:37:39 +0200

    repair channels m

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 
 web: daphne blog.asgi:application --port $PORT --bind 0.0.0.0 -v2
-chatworker: python manage.py runworker --settings=blog.settings -v2
+celery: celery -A blog.celery worker --pool=solo -l info
+celerybeat: celery -A blog beat -l info
---
+++ staticfiles/src/vue/dist/js/8.js
@@ -0,0 +1,15 @@
+((typeof self !== 'undefined' ? self : this)["webpackJsonp"] = (typeof self !== 'undefined' ? self : this)["webpackJsonp"] || []).push([[8],{
+
+/***/ "./views/accounts/Settings.vue":
+/*!*************************************!*\
+  !*** ./views/accounts/Settings.vue ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var C_Users_papad_OneDrive_Dokumenty_MEGA_MEGAsync_AppDev_10_02_22_quizCityDeployed_blog_frontend_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/vue-loader-v16/dist/exportHelper.js */ \"./node_modules/vue-loader-v16/dist/exportHelper.js\");\n/* harmony import */ var C_Users_papad_OneDrive_Dokumenty_MEGA_MEGAsync_AppDev_10_02_22_quizCityDeployed_blog_frontend_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(C_Users_papad_OneDrive_Dokumenty_MEGA_MEGAsync_AppDev_10_02_22_quizCityDeployed_blog_frontend_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_0__);\nconst script = {}\n\n\nconst __exports__ = /*#__PURE__*/C_Users_papad_OneDrive_Dokumenty_MEGA_MEGAsync_AppDev_10_02_22_quizCityDeployed_blog_frontend_node_modules_vue_loader_v16_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_0___default()(script, [['__file',\"views/accounts/Settings.vue\"]])\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (__exports__);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi92aWV3cy9hY2NvdW50cy9TZXR0aW5ncy52dWUuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi92aWV3cy9hY2NvdW50cy9TZXR0aW5ncy52dWU/ZGRlMSJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBzY3JpcHQgPSB7fVxuXG5pbXBvcnQgZXhwb3J0Q29tcG9uZW50IGZyb20gXCJDOlxcXFxVc2Vyc1xcXFxwYXBhZFxcXFxPbmVEcml2ZVxcXFxEb2t1bWVudHlcXFxcTUVHQVxcXFxNRUdBc3luY1xcXFxBcHBEZXZcXFxcMTAuMDIuMjJcXFxccXVpekNpdHlEZXBsb3llZFxcXFxibG9nXFxcXGZyb250ZW5kXFxcXG5vZGVfbW9kdWxlc1xcXFx2dWUtbG9hZGVyLXYxNlxcXFxkaXN0XFxcXGV4cG9ydEhlbHBlci5qc1wiXG5jb25zdCBfX2V4cG9ydHNfXyA9IC8qI19fUFVSRV9fKi9leHBvcnRDb21wb25lbnQoc2NyaXB0LCBbWydfX2ZpbGUnLFwidmlld3MvYWNjb3VudHMvU2V0dGluZ3MudnVlXCJdXSlcblxuZXhwb3J0IGRlZmF1bHQgX19leHBvcnRzX18iXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQSIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./views/accounts/Settings.vue\n");
+
+/***/ })
+
+}]);
