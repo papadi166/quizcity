a105000b9243841005f586d3e66a4aaac4582cd7
papadi166 <papadi166@gmail.com>
2022-05-22 19:37:51 +0200

    repair channels m

--- blog/wsgi.py
+++ blog/wsgi.py
@@ -13,5 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
 
+import django
+django.setup()
+
 application = get_wsgi_application()
 
