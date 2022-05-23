10c88455b730ef1fdb0d6949c971394d728c06f9
papadi166 <papadi166@gmail.com>
2022-05-23 11:58:17 +0200

    repair channels m

--- blog/asgi.py
+++ blog/asgi.py
@@ -4,11 +4,12 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 import os
 import django
 
-from django.core.asgi import get_asgi_application
+
 
 from channels.routing import ProtocolTypeRouter
 from channels.auth import AuthMiddlewareStack
